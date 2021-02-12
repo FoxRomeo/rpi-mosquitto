e68cf8df6673633f703423d13c3c83d972dcb41b
Oliver Faßbender <git@intrepid.de>
2021-02-12 09:54:39 +0000

    changed build process

--- get_version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-VERSION=`curl -s "http://mosquitto.org/download/" | grep 'a href="https://mosquitto.org/files/source/mosquitto-' | sed -n -e 's/^.*\.tar\.gz">mosquitto-'//p | sed -n -e 's/\.tar\.gz<\/a> .*$'//p`
-
-echo ${VERSION}
